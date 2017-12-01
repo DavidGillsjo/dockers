bf501403218506b7235e0d83a4e057c0a02a434b
David Gillsjö <davidg@maths.lth.se>
2017-12-01 14:11:39 +0100

    * Do not create user for root, will fail

--- PSPNet/build.sh
+++ PSPNet/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-docker build\
+
+#Do not build container for sudo user, use default args.
+if [ $(id -g $USER) == 0 ] ; then
+  BUILD_ARGS=""
+else
+  BUILD_ARGS="--build-arg gid=$(id -g)\
+              --build-arg uid=$(id -u)\
+              --build-arg user=$USER"
+fi
+
+docker build ${BUILD_ARGS}\
   -t pspnet\
-  --build-arg gid=$(id -g)\
-  --build-arg uid=$(id -u)\
-  --build-arg user="$USER"\
-  "$@" .
+   "$@" .
