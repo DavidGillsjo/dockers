a8c57b2b68235feeabf2876628d8d3c31b624af1
David Gillsjö <davidg@maths.lth.se>
2017-12-01 13:03:58 +0100

    * Try to run docker as local user

--- PSPNet/build.sh
+++ PSPNet/build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker build -t pspnet .
+docker build\
+  -t pspnet\
+  --build-arg gid=$(id -g)\
+  --build-arg uid=$(id -u)\
+  --build-arg user="$USER"\
+  .
--- PSPNet/run_local.sh
+++ PSPNet/run_local.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #Usage: [ENV_OPTS] ./run [CMD] [ARGS]
+MYUID=$(id -u $USER)
+MYGID=$(id -g $USER)
 nvidia-docker run --rm -it \
         --name=pspnet\
         -v "${DATA-/tmp/data}:/workspace/data:rw"\
         -p "8888:8888"\
-        pspnet "$@"
+        -u "$MYUID:$MYGID"\
+        "${IMAGE-pspnet}" "$@"
