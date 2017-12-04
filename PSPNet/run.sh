104ba96e2cba7d217ee4cb374308c6f3d0c6601f
David Gillsjö <davidg@maths.lth.se>
2017-12-04 15:50:56 +0100

    * Mount home directoy, not very elegant

--- PSPNet/run.sh
+++ PSPNet/run.sh
@@ -12,9 +12,21 @@ else
   USER_OPT="-u $(id -u $DUSER):$(id -g $DUSER)"
 fi
 
+# Use $HOME unless run with sudo
+if [ -z $DHOME ] ; then
+  if [ $(id -g $USER) == 0 ] ; then
+    HOME_OPT=""
+  else
+    HOME_OPT="-v ${HOME}:/workspace/host_home:rw"
+  fi
+else
+  HOME_OPT="-v ${DHOME}:/workspace/host_home:rw"
+fi
+
 nvidia-docker run --rm -it \
         --name=pspnet\
         -v "${DATA-/tmp/data}:/workspace/data:rw"\
         -p "8888:8888"\
         ${USER_OPT}\
+        ${HOME_OPT}\
         "${IMAGE-pspnet}" "$@"
